6a7915787700c71e45e721a6c49a9611abae792e
HungNguyen3205 <namhung03022005@gmail.com>
2024-10-01 22:58:37 +0700

    Hung

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WalletCard from './component/WalletCard'
+
+function App() {
+  return (
+    <div className="App">
+      <WalletCard/>
+    </div>
+  );
+}
+
+export default App;
